3044ab9b7a08b5aaf6ae330cdbf895aa519ed290
Lucas-note <lgbs@cesar.school>
2023-07-05 23:55:26 -0300

    docs: Adding documentation string to bot commands

--- emprestaBot.py
+++ emprestaBot.py
@@ -1,5 +1,4 @@
 from os import linesep
-import json
 import discord
 import requests
 from discord.ext import commands
@@ -9,14 +8,14 @@ path = 'http://127.0.0.1:8000/estoque'
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.command()
 async def get_item(ctx, item):
+    '''
+    Exibe informações do item escolhido 
+    >get_item [nome_do_item]
+    '''
     response = requests.get(url=f'{path}/get-item/{item}').json()
     string = f'{item} no GARAGino'
 
@@ -27,6 +26,10 @@ async def get_item(ctx, item):
 
 @bot.command()
 async def emprestar(ctx, item):
+    '''
+    Pega um item emprestado, tirando-o do estoque
+    >emprestar [nome_do_item]
+    '''
     response = requests.put(url=f'{path}/emprestar?nome_item={item}')
     if 'Erro' not in response:
         await ctx.send(f'{item} emprestado! Não esqueça de pega-lo!')
@@ -35,6 +38,10 @@ async def emprestar(ctx, item):
 
 @bot.command()
 async def devolver(ctx, item):
+    '''
+    Devolve um item que foi pego, colocando-o no estoque
+    >devolver [nome_do_item]
+    '''
     response = requests.put(url=f'{path}/devolver?nome_item={item}')
     if 'Erro' not in response:
         await ctx.send(f'{item} Devolvido! Não esqueça de entrega-lo!')
